45259b7d8e21529215c014d71d65b5d4ea6f9f52
Lance Ju <lance@crenolab.com>
2014-12-05 11:39:47 +0800

    Add the publish message test(including text, image and voice).

--- test/test.message.js
+++ test/test.message.js
@@ -1,7 +1,11 @@
+var fs 			= require( 'fs' );
 var should 		= require( 'should' );
+
 var testConfig  = require( './config' );
 var rongSDK 	= require( '../index' );
 
+var base64Voice	= new Buffer( fs.readFileSync( './testvoice.amr' ) ).toString( 'base64' );
+var base64Image	= new Buffer( fs.readFileSync( './raindrop.jpg' ) ).toString( 'base64' );
 
 describe( 'Message Test', function() {
 	before( function( done ) {
@@ -14,16 +18,37 @@ describe( 'Message Test', function() {
 		done();
 	} );
 
-	describe( 'Send Message', function() {
+	describe( 'Publish Message', function() {
 		it( 'Text message: should return OK', function( done ) {
-			rongSDK.message.publish( testConfig.message.fromUserId, testConfig.message.toUserId, 'RC:TxtMsg', testConfig.message.textMsg, function( err, resultText ) {
+			var textMessageObject = { content : testConfig.message.textMsg };
+			rongSDK.message.publish( testConfig.message.fromUserId, testConfig.message.toUserId, 'RC:TxtMsg', JSON.stringify( textMessageObject ), function( err, resultText ) {
 				should.not.exists( err );
-				console.log( resultText );
 				var result = JSON.parse( resultText );
 				result.code.should.equal( 200 );
 				done();
 			} );
 		} );
+
+		it( 'Image message: should return OK', function( done ) {
+			var imageMessageObject = { content : base64Image, imageKey : 'http://lanceju-com.qiniudn.com/raindrop.jpg' };
+			rongSDK.message.publish( testConfig.message.fromUserId, testConfig.message.toUserId, 'RC:ImgMsg', JSON.stringify( imageMessageObject ), function( err, resultText ) {
+				should.not.exists( err );
+				var result = JSON.parse( resultText );
+				result.code.should.equal( 200 );
+				done();
+			} );
+		} );
+
+		it( 'Voice message: should return OK', function( done ) {
+			var voiceMessageObject = { content : base64Voice, duration : 4 };
+			rongSDK.message.publish( testConfig.message.fromUserId, testConfig.message.toUserId, 'RC:VcMsg', JSON.stringify( voiceMessageObject ), function( err, resultText ) {
+				should.not.exists( err );
+				var result = JSON.parse( resultText );
+				result.code.should.equal( 200 );
+				done();
+			} );
+		} );
+
 	} );
 
 
@@ -32,7 +57,6 @@ describe( 'Message Test', function() {
 	// 	it( 'Should return OK', function( done ) {
 	// 		rongSDK.message.broadcast( testConfig.message.fromUserId, 'RC:TxtMsg', testConfig.message.textMsg, function( err, resultText ) {
 	// 			should.not.exists( err );
-	// 			console.log( resultText );
 	// 			var result = JSON.parse( resultText );
 	// 			result.code.should.equal( 200 );
 	// 			done();
