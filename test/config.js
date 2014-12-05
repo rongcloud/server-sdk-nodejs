784943c1278e479382c7f26f1f65aa53bb28517d
Lance Ju <lance@crenolab.com>
2014-12-05 19:47:32 +0800

    Add more chat room infos.

--- test/config.js
+++ test/config.js
@@ -14,5 +14,8 @@ module.exports = {
 	group : {
 		userId : '5460603c1002a6e311f89e7f',
 		groupIdNamePairs : { 'ProgrammerGroup1' : '程序猿交流群1', 'DriverGroup1' : '赛车手爱好者2群' }
+	},
+	chatroom : {
+		chatroomIdNamePairs : { 'EatingFans' : '吃货大本营', 'ProCylcing' : '骑行专家', 'DriodGeek' : '手机极客', 'HackerHome' : '黑客之家' }
 	}
 }
