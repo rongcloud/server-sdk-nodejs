1b35aca70e54e02aedc0e4bfeb03463f604817fd
Lance Ju <lance@crenolab.com>
2015-06-10 15:06:48 +0800

    Add the testing on refresh group API. Fix the wrongg ref in dismiss group API

--- test/test.group.js
+++ test/test.group.js
@@ -62,7 +62,7 @@ describe( 'Group Test', function() {
 
 	describe( 'Dissmiss Group', function() {
 		it( 'Dismiss group(' + groupNames[0] + '): should return OK', function( done ) {
-			rongSDK.group.quit( testConfig.group.userId, groupIDs[0], function( err, resultText ) {
+			rongSDK.group.dismiss( testConfig.group.userId, groupIDs[0], function( err, resultText ) {
 				should.not.exists( err );
 				var result = JSON.parse( resultText );
 				result.code.should.equal( 200 );
@@ -71,7 +71,7 @@ describe( 'Group Test', function() {
 		} );
 
 		it( 'Dismiss group(' + groupNames[1] + '): should return OK', function( done ) {
-			rongSDK.group.quit( testConfig.group.userId, groupIDs[1], function( err, resultText ) {
+			rongSDK.group.dismiss( testConfig.group.userId, groupIDs[1], function( err, resultText ) {
 				should.not.exists( err );
 				var result = JSON.parse( resultText );
 				result.code.should.equal( 200 );
@@ -80,7 +80,7 @@ describe( 'Group Test', function() {
 		} );
 
 		it( 'Dismiss group(MyGroupName): should return OK', function( done ) {
-			rongSDK.group.quit( testConfig.group.userId, 'MyGroupID', function( err, resultText ) {
+			rongSDK.group.dismiss( testConfig.group.userId, 'MyGroupID', function( err, resultText ) {
 				should.not.exists( err );
 				var result = JSON.parse( resultText );
 				result.code.should.equal( 200 );
@@ -89,5 +89,19 @@ describe( 'Group Test', function() {
 		} );
 
 	} );
+
+
+	describe( 'Refresh Group', function() {
+    var newGroupName = groupNames[0];
+		it( 'Refresh group(' + groupNames[0] + ') to be ' + newGroupName + ': should return OK', function( done ) {
+			rongSDK.group.refresh( groupIDs[0], newGroupName, function( err, resultText ) {
+				should.not.exists( err );
+				var result = JSON.parse( resultText );
+				result.code.should.equal( 200 );
+				done();
+			} );
+		} );
+  });
+
 
 } );
